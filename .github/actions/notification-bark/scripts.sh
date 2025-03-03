04509dfc7c133c981e6efc13ee9478cb215b5116
rtx3 <r@rtx3.com>
2025-03-03 17:25:43 +0800

    feat: 添加自定义 Bark 通知图标和分组
    允许用户自定义 Bark 通知图标和分组，增强通知的个性化和组织性。

---
+++ .github/actions/notification-bark/scripts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+red='\e[91m'
+green='\e[92m'
+cyan='\e[96m'
+none='\e[0m'
+
+if [[ -z $INPUT_KEY ]]; then
+  echo -e "${red}ERROR: the KEY field is required${none}" >&2
+  exit 1
+fi
+
+if [[ -n $INPUT_HOST ]]; then
+  host="$INPUT_HOST"
+  echo -e "${cyan}Use custom Bark server hostname${none}: ${host}"
+else
+  host="https://api.day.app"
+fi
+
+if [[ -n $INPUT_ICON ]]; then
+  icon="$INPUT_ICON"
+  echo -e "${cyan}Use custom icon ${icon}"
+else
+  icon="https://day.app/assets/images/avatar.jpg"
+fi
+
+
+
+request_url="${host}/${INPUT_KEY}/"
+request_body="title=${INPUT_TITLE}&body=${INPUT_BODY}&sound=${INPUT_SOUND}&isArchive=${INPUT_IS_ARCHIVE}&url=${INPUT_URL}&automaticallyCopy=${INPUT_AUTOMATICALLY_COPY}&copy=${INPUT_COPY}&icon=${icon}&group=${INPUT_GROUP}"
+
+echo -e "${cyan}Request url${none}: ${request_url}"
+echo -e "${cyan}Request body${none}:\n${request_body//&/\\n}"
+
+http_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d "${request_body}" "${request_url}")
+if [[ $http_code == 200 ]]; then
+  echo -e "${green}Notification sent successfully${none}"
+else
+  echo -e "${red}Request error! The HTTP code is ${http_code}${none}" >&2
+  exit 1
+fi
